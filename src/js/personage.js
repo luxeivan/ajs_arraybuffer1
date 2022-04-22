aff0880b8ad7c1feb8045ff3f7c5eec16ba696c8
luxeivan <luxeivan@gmail.com>
2022-04-22 14:44:39 +0300

    changed code

--- src/js/personage.js
+++ src/js/personage.js
@@ -1,9 +1,26 @@
 export default class Personage {
-  getAttack(attack, distance) {
-    return attack - attack / 10 * (distance - 1);
+  constructor() {
+    this.attack = 0;
+    this.stoned = false;
+    this.distance = 2;
   }
 
-  getStoned(attack, distance) {
-    return Math.round(this.getAttack(attack, distance) - Math.log2(distance) * 5);
+  get attack() {
+    const attack = this.attack - this.attack / 10 * (this.distance - 1);
+    if (this.stoned) {
+      attack = Math.round(attack - Math.log2(this.distance) * 5);
+    }
+    return attack
+  }
+  set attack(attack) {
+    this.attack = attack;
+  }
+
+  get stoned() {
+    return this.stoned;
+  }
+
+  set stoned(stoned) {
+    this.stoned = stoned;
   }
 }
